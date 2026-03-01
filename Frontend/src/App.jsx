2e6ddafff3bcf359a5a13046a2a2c342572ec95c
prakash69-code <neupaneprakash9999@gmail.com>
2026-03-01 21:32:57 +0545

    Add Movie Card component

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import MovieCard from "./components/MovieCard";
+
+function App() {
+  return (
+    <>
+      <MovieCard movie={{ title: "Tim's Film", release_date: "2024" }} />
+      <MovieCard movie={{ title: "Joes Film", release_date: "2024" }} />
+    </>
+  );
+}
+
+export default App;
