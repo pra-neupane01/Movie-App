a776a24b203bd609a0c1055228927f0ff24d3d6c
prakash69-code <neupaneprakash9999@gmail.com>
2026-03-02 08:50:18 +0545

    Integrate conditional Rendering + Home Page & .map()

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
-import MovieCard from "./components/MovieCard";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <MovieCard movie={{ title: "Tim's Film", release_date: "2024" }} />
-      <MovieCard movie={{ title: "Joes Film", release_date: "2024" }} />
+      <Home />
     </>
   );
 }
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import MovieCard from "../components/MovieCard";
+
+function Home() {
+  const movies = [
+    {
+      id: 1,
+      title: "Inception",
+      release_date: "2010-07-16",
+    },
+    {
+      id: 2,
+      title: "The Matrix",
+      release_date: "1999-03-31",
+    },
+    {
+      id: 3,
+      title: "Interstellar",
+      release_date: "2014-11-07",
+    },
+    {
+      id: 4,
+      title: "The Dark Knight",
+      release_date: "2008-07-18",
+    },
+  ];
+
+  const handleSearch = () => {};
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="search for movies..."
+          className="search-input"
+        />
+
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid"></div>
+      {movies.map((movie) => (
+        <MovieCard movie={movie} key={movie.id} />
+      ))}
+    </div>
+  );
+}
+
+export default Home;
