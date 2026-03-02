1eb4918bb1ac6c518f004ce275c27910ae46332e
prakash69-code <neupaneprakash9999@gmail.com>
2026-03-02 20:56:53 +0545

    Integrate CSS styling

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./css/App.css";
 import Home from "./pages/Home";
 import Favourites from "./pages/Favourites";
 import NavBar from "./components/NavBar";
--- Frontend/src/pages/Favourites.jsx
+++ Frontend/src/pages/Favourites.jsx
@@ -1,10 +1,11 @@
-function Favourite() {
+import "../css/Favourites.css";
+function Favourites() {
   return (
-    <div className="favourite-empty">
+    <div className="favourites-empty">
       <h2>No Favourite Movies yet.</h2>
       <p>Start adding movies to your favourite and they will appear here.</p>
     </div>
   );
 }
 
-export default Favourite;
+export default Favourites;
