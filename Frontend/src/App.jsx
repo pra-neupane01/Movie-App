45e1971eb82379c12b0e611d2c441d9b9d03f5e4
prakash69-code <neupaneprakash9999@gmail.com>
2026-03-02 20:40:21 +0545

    Integrate Page Routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,20 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Favourites from "./pages/Favourites";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
---
+++ Frontend/src/pages/Favourites.jsx
@@ -0,0 +1,10 @@
+function Favourite() {
+  return (
+    <div className="favourite-empty">
+      <h2>No Favourite Movies yet.</h2>
+      <p>Start adding movies to your favourite and they will appear here.</p>
+    </div>
+  );
+}
+
+export default Favourite;
